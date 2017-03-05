3e732226f2f766e2e7c8a40313ec42e75eb79068
Nicole <nicole@datrium.com>
2017-03-05 19:51:10 +0100

    no message

--- simple_login/settings.py
+++ simple_login/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+ALLOWED_HOSTS = ['*']
 
 TEMPLATES = [
     {
